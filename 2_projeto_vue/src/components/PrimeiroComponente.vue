15e3023ffe11541a36137771ec0a8c1842ae8f53
Galdinowski <arthurlazarogaldino@gmail.com>
2024-06-27 16:03:47 -0300

    Usando MultiplosEventos

--- 2_projeto_vue/src/components/PrimeiroComponente.vue
+++ 2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -2,14 +2,23 @@
     <div>
         <h1>Ola vue</h1>
         <p>Meu nome eh: {{ name }}</p>
+        <MultiplosEventos/>
     </div>
     
 </template>
 
 <script>
+import MultiplosEventos from './MultiplosEventos.vue';
+
     export default{
     
         name:'PrimeiroComponente',
+        components: {
+
+                MultiplosEventos
+
+        },
+
         data()
         {
             return {
